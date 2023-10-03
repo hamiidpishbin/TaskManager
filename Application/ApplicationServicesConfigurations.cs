10cb37d80dfaf45b06c237bceebabddbf6ca610e
Hamid Pishbin <hamidpishbin89@gmail.com>
2023-10-03 20:22:26 +0330

    Add Interfaces to Dependency Container

--- Application/ApplicationServicesConfigurations.cs
+++ Application/ApplicationServicesConfigurations.cs
@@ -1,3 +1,5 @@
+using Application.Interfaces;
+using Application.Services;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Application;
@@ -6,6 +8,8 @@ public static class ApplicationServicesConfigurations
 {
   public static IServiceCollection AddApplicationServices(this IServiceCollection services)
   {
+    services.AddScoped<ISprintService, SprintService>();
+    services.AddScoped<ITaskService, TaskService>();
     return services;
   }
 }
